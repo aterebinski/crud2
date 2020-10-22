5e385f4ecd93438382feb745f1df51ce30fa2250
Adam Terebiński <aterebinski@wp.pl>
2020-10-22 21:36:46 +0200

    Routing configuration. Repair aplication.scss.

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,5 @@
 Rails.application.routes.draw do
-  get 'posts/index'
-  get 'posts/show'
-  get 'posts/edit'
-  get 'posts/add'
+  resources 'posts'
+  root 'posts#index'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
