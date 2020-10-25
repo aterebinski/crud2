5f9c323f5bc95801f52db009539ee4490b214691
Adam Terebiński <aterebinski@wp.pl>
2020-10-25 20:02:55 +0100

    Adding Post. Updating Post.

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -7,9 +7,29 @@ class PostsController < ApplicationController
   end
 
   def edit
+    @post = Post.find(params[:id])
+  end
+
+  def update
+    @post = Post.find params[:id]
+    @post.update(post_paramters)
+    @post.save
+    redirect_to posts_path
   end
 
   def new
     @post = Post.new
+  end
+
+  def create
+    @post = Post.new(post_paramters)
+    @post.save
+    redirect_to posts_path
+  end
+
+  private
+
+  def post_paramters
+    params.require(:post).permit(:title, :author, :body, :published)
   end
 end
